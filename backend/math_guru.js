d21b4aeac778c60f413dccbc8c503c21234e381f
ddadaya <vovamaksakov52@gmail.com>
2021-05-18 21:31:25 +0300

    first Vova commit

--- backend/math_guru.js
+++ backend/math_guru.js
@@ -1,46 +1,54 @@
-const operators = ['*', '+', '-']
-let args = []
-let operator
 
+function all(i){
 
-function genNum(num = 100, min = 0){
-  return Math.floor(Math.random() * (num-min) + min);
-}
-
-function genOperator(){
-  operator = operators[Math.floor(Math.random()*3)]
-}
-
-function genResult(){
-  switch(operator){
-    case operators[0]:
-      args.push(genNum(10, 2));
-      args.push(genNum(10, 2));
-      return args[0] * args[1]
-    case operators[1]:
-      args.push(genNum());
-      args.push(genNum());
-      return args[0] + args[1]
-    case operators[2]:
-      args.push(genNum());
-      args.push(genNum());
-      return args[0] - args[1]
+  const operators = ['*', '+', '-']
+  let args = []
+  let operator
+  
+  
+  function genNum(num = 100, min = 0){
+    return Math.floor(Math.random() * (num-min) + min);
   }
+  
+  function genOperator(){
+    operator = operators[Math.floor(Math.random()*3)]
+  }
+  
+  function genResult(){
+    switch(operator){
+      case operators[0]:
+        args.push(genNum(10, 2));
+        args.push(genNum(10, 2));
+        return args[0] * args[1]
+      case operators[1]:
+        args.push(genNum());
+        args.push(genNum());
+        return args[0] + args[1]
+      case operators[2]:
+        args.push(genNum());
+        args.push(genNum());
+        return args[0] - args[1]
+    }
+  }
+  
+  function genNextTask(){
+    genOperator()
+    genResult()
+   // task.task_num++
+  }
+  
+
+  genNextTask()
+  const task = {
+      operator: operator, 
+      result: genResult(),
+      first_arg: args[0],
+      second_arg: args[1],
+     // task_num: (i)=>{return i++}
+     task_num: i
+      
+  }
+  return task
 }
 
-function genNextTask(){
-  genOperator()
-  genResult()
-}
-
-genNextTask()
-
-const task = {
-    operator: operator, 
-    result: genResult(),
-    first_arg: args[0],
-    second_arg: args[1],
-    task_num: 1
-}
-
-module.exports = task;
+module.exports = all
--- frontend/src/pages/page0.js
+++ frontend/src/pages/page0.js
@@ -6,9 +6,32 @@ import { Button } from '@sberdevices/ui/components/Button/Button';
 import { Container } from '@sberdevices/plasma-ui/components/Grid';
 import { Image } from '@sberdevices/ui/components/Image/Image';
 import Scene from '../components/menu'
+//import  {genNextTask} from '../math_guru_logic/math_guru'
 
 const Page0 = () =>{
-  return(<div>MATH GURU</div>)
+ 
+  function genNextTask(){
+   
+    fetch('http://localhost:3000/math_guru')
+    .then(res=>res)
+  //  .then(data=>console.log(data))
+   //.catch(error=>console.log(error))
+    
+  }
+  return(
+  <>
+    
+    <Container>
+      <Button onClick={genNextTask}>next task</Button>
+           <div>
+             {}
+             </div>
+    </Container>
+
+
+
+    </>
+    )
 }
 
 export default Page0
